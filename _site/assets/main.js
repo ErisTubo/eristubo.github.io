d64a16944897b6e4e3974164d911a6babff5ec94
Eris <eris.tubo.@gmail.com>
2018-01-28 17:09:18 +0100

    WIP - Front
    WIP Front

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -3,4 +3,17 @@ $( document ).ready(function() {
       $(this).toggleClass('header');
       $(this).next('section').toggleClass('show');
     });
+    $('img[alt="FIN"]').parent().addClass('hidemeta');
+    $('img[alt="FIN"]').hover(function() {
+      $(this).parent().removeClass('hidemeta');
+      $(this).addClass('hideimg');
+    }, function() {
+      $(this).parent().addClass('hidemeta');
+      $(this).removeClass('hideimg');
+    });
+    $('img[alt="WIP"]').hover(function() {
+      $(this).parent().addClass('hidemeta');
+    }, function() {
+      $(this).parent().removeClass('hidemeta');
+    });
 });
